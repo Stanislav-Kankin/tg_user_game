2c6a33dcf358f267a2dbd3b14c37017fe1f4eba7
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-10-27 18:56:22 +0300

    test

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,9 @@
-from random import randint
-from datetime import datetime, timedelta
-import pytz
+
 from typing import Callable, Awaitable, Any
 
-from aiogram import Bot, Dispatcher, BaseMiddleware, F
+from aiogram import Bot, Dispatcher, BaseMiddleware
 from aiogram.client.default import DefaultBotProperties
-from aiogram.types import (
-    Message, Update, WebAppInfo, CallbackQuery, InlineKeyboardButton
-    )
+from aiogram.types import Message, WebAppInfo
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -92,148 +88,21 @@ app.mount("/static", StaticFiles(directory=config.STATIC_PATH), name="static")
 
 @dp.message(CommandStart())
 async def start(message: Message, user: User):
-    """
-    Обработчик команды /start.
-
-    :param message: Сообщение.
-    :param user: Пользователь.
-    """
-    dict_markup = None
-    dt_time_cmd_start = datetime.now(pytz.utc)
-    if 1 <= user.number_of_tries <= 5:
-        dict_markup = (
-            InlineKeyboardBuilder()
-            .button(
-                text="🍀 Испытай свою удачу!",
-                web_app=WebAppInfo(url=config.WEBAPP_URL))
-        ).as_markup()
-    elif user.number_of_tries <= 0:
-        dict_markup = (
-            InlineKeyboardBuilder()
-            .row(
-                InlineKeyboardButton(
-                    text="🤑 Добавить ящики сейчас(КУПИТЬ)!",
-                    callback_data="pay"
-                )
-            )
-            .row(
-                InlineKeyboardButton(
-                    text="🥰 Пригласи друга в группу и получишь +1 попытку!",
-                    callback_data="friend"
-                )
-            )
-        ).as_markup()
+    dict_markup = (
+        InlineKeyboardBuilder()
+        .button(
+            text="🎮 Играть!",
+            web_app=WebAppInfo(url=config.WEBAPP_URL))
+    ).as_markup()
 
     await message.answer(
-        f"🎁 <b>Ящиков открыто:</b> <code>{user.luckyboxes['count']}</code> "
-        f"(+<code>{user.luckyboxes['cash']}</code>)\n"
-        f"🎲 Осталось ящиков <b>{user.number_of_tries}</b>.\n",
-
+        "Привет, мы можем поиграть, если хочешь!",
         reply_markup=dict_markup
-    )
-    user.cmd_str = dt_time_cmd_start
-    await user.save()
-    if user.number_of_tries < 5 and user.next_usage > user.cmd_str:
-        user.number_of_tries = user.number_of_tries
-    elif user.number_of_tries < 5 and user.next_usage <= user.cmd_str:
-        user.number_of_tries = 5
-        await user.save()
-
-
-@dp.callback_query(F.data == "pay")
-async def pay(callback_query: CallbackQuery):
-    await callback_query.message.answer(
-        "тут будет логика получения оплаты."
-    )
-
-
-@dp.callback_query(F.data == "friend")
-async def invite_friend(callback_query: CallbackQuery):
-    await callback_query.message.answer(
-        "тут логика приглашения друга в целевую группу."
-    )
 
 
 @app.get("/")
 async def root(request: Request):
-    """
-    Обработчик корневого маршрута.
-
-    :param request: Запрос.
-    :return: Ответ с шаблоном index.html.
-    """
     return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.post("/open-box")
-async def open_box(request: Request):
-    """
-    Обработчик маршрута для открытия ящика.
-
-    :param request: Запрос.
-    :return: JSON ответ с результатом открытия ящика.
-    """
-    authorization = request.headers.get("Authentication")
-    try:
-        data = safe_parse_webapp_init_data(bot.token, authorization)
-    except ValueError:
-        return JSONResponse({"success": False, "error": "Unauthorized"}, 401)
-
-    dt_current_datetime = datetime.now(pytz.utc)
-    dt_next_use = dt_current_datetime + timedelta(seconds=30)
-
-    i_cash = randint(0, 1000)
-    user = await User.filter(id=data.user.id).first()
-
-    if user.number_of_tries == 0:
-        return JSONResponse(
-            {"success": False,
-             "error": "Невозможно открыть сейчас. Нет ящиков. 😢",
-             "cash": -1}
-            )
-
-    user.luckyboxes["count"] += 1
-    user.luckyboxes["cash"] += i_cash
-    user.number_of_tries -= 1
-    user.time_of_use = dt_current_datetime
-    user.next_usage = dt_next_use
-
-    await user.save()
-
-    return JSONResponse({"success": True, "cash": i_cash})
-
-
-@app.post("/get-stats")
-async def get_stats(request: Request):
-    """
-    Обработчик запроса для получения статистики пользователя.
-
-    Args:
-        request (Request): Объект запроса, содержащий заголовки и данные.
-
-    Returns:
-        JSONResponse: Ответ в формате JSON, содержащий статистику пользователя.
-
-    Raises:
-        JSONResponse: Возвращает ошибку 401, если заголовок
-        Authentication недействителен.
-        JSONResponse: Возвращает ошибку 404, если пользователь не найден.
-    """
-    authorization = request.headers.get("Authentication")
-    try:
-        data = safe_parse_webapp_init_data(bot.token, authorization)
-    except ValueError:
-        return JSONResponse({"success": False, "error": "Unauthorized"}, 401)
-
-    user = await User.filter(id=data.user.id).first()
-    if user is None:
-        return JSONResponse({"success": False, "error": "User not found"}, 404)
-
-    return JSONResponse({
-        "success": True,
-        "wins": user.luckyboxes["cash"],
-        "total_boxes": user.luckyboxes["count"]
-    })
 
 
 async def main():
@@ -267,4 +136,4 @@ async def webhook(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
--- src/models/user.py
+++ src/models/user.py
@@ -4,9 +4,6 @@ from tortoise.models import Model
 
 class User(Model):
     id = fields.BigIntField(pk=True)
-    username = fields.CharField(max_length=32, null=True)
-    luckyboxes = fields.JSONField(default={"count": 0, "cash": 0})
-    time_of_use = fields.DatetimeField(null=True)
-    next_usage = fields.DatetimeField(null=True)
-    number_of_tries = fields.IntField(default=5, null=True)
-    cmd_str = fields.DatetimeField(null=True)
+    username = fields.CharField(max_length=50, null=True)
+    games_played = fields.IntField(default=0)  # Количество сыгранных игр
+    total_score = fields.IntField(default=0)   # Общее число очков победы
