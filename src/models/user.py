b30472bf7dff45e781242c8a16607a301b467f0d
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-11-04 08:03:18 +0300

    ви

--- src/models/user.py
+++ src/models/user.py
@@ -3,10 +3,7 @@ from tortoise.models import Model
 
 
 class User(Model):
-    """
-    Модель пользователя для хранения информации о пользователях.
-    """
-    i_id = fields.BigIntField(pk=True)
-    s_username = fields.CharField(max_length=50, null=True)
-    i_games_played = fields.IntField(default=0)  # Количество сыгранных игр
-    i_total_score = fields.IntField(default=0)   # Общее число очков победы
+    id = fields.BigIntField(pk=True)
+    username = fields.CharField(max_length=50, null=True)
+    games_played = fields.IntField(default=0)  # Количество сыгранных игр
+    total_score = fields.IntField(default=0)   # Общее число очков победы
